ad042df8d400b36fe1f1bb7fbeb1e6a82432d416
adityahazarika <46670592+adityahazarika@users.noreply.github.com>
2023-03-23 14:59:38 +0530

    Update iterator.js

--- iterator.js
+++ iterator.js
@@ -1,24 +1,30 @@
-
 function someObj() {
     let a = 0;
     return {
         next() {
-            if (a > 3) {
+            if(a>10){
                 return {
                     "done": true,
                     "value": a
-                }
+                }            
             }
             a = a + 1;
             return {
                 "done": false,
-                "value": a
+                "value": a-1
             }
         }
     }
 }
 
+
 const myIterable = {};
 myIterable[Symbol.iterator] = someObj
 
 console.log(9,...myIterable)
+
+
+//Now myIterable Object can be iterated over for..of construct
+//for (const i of myIterable) {
+  //  console.log(i)
+//}
